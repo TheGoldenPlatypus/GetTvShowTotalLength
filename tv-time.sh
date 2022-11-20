18d7efbf3a3e65ffca916fe5d5420400b0483e71
Omer <omerrath@outlook.de>
2022-11-20 16:32:57 +0200

    comments been added to tv-time.sh

--- tv-time.sh
+++ tv-time.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+# Copyright 2022 Omer Ratsaby
+#
+# This is a BASH script that uses C# application to get the
+# total length of given TV shows and find the 
+# shortest and the longest from a given list.
+#
+# Please note: a path to GetTvShowTotalLength C# application (binary) 
+# should be passed via environment variable GET _TVSHOW_ TOTAL _LENGTH_ BIN.
+#
+# By default, the variable is not set. Please set this variable in this way:
+# export GET_TVSHOW_TOTAL_LENGTH_BIN ="./GetTvShowTotalLength/bin/Debug/net6.0/GetTvShowTotalLength"
+#
+# then you can run the script with the following command:
+# ./tv-time.sh < tv-shows.txt
+
 echoerr() { 
     echo "$@" 1>&2; 
 }
+
 
 function getUUID()
 {
@@ -36,12 +52,25 @@ function getUUID()
     echo
 }
 
+
 function makeTempFolder() {
     folderId=$(getUUID);
     TEMP_FOLDER_PATH="./myTemp$folderId";
     mkdir "$TEMP_FOLDER_PATH/" 2> /dev/null;
 }
 
+#######################################
+# Find the Maximal&Minimal Length of a TV-show
+# using a parallel calls to a c# app 
+# this recursive fucntion implements "divide and conquer" algorithm
+# to achieve it in O(log(n)) time complexity.
+# GLOBALS:
+#  An Array containing TV-shows names
+# ARGUMENTS:
+#   Indexes of the first and the last elements of an array
+# OUTPUTS:
+#   The shortest/longest TV-shows and their durations
+#######################################
 function findMinMax { 
     local leftIndex=$1;
     local rightIndex=$2;
@@ -54,6 +83,9 @@ function findMinMax {
     local dif=$((rightIndex-leftIndex))
     local absDif=${dif#-}
 
+    # Base Case - if the length of the array is less or equal to 1
+    # determine the min& max shows and updates the local varibles. 
+    # this block also handles missing data occurrences
     if [[ $absDif -le 1 ]]
     then
         local leftShow=${arr[$leftIndex]};
@@ -98,8 +130,8 @@ function findMinMax {
             max=$leftTime;
             maxShow=$leftShow;
         fi
-
-        # echo *indexes: $leftIndex $rightIndex, $minShow $min $maxShow $max, exitCodeLeft=$exitCodeLeft exitCodeRight=$exitCodeRight>> outpulMinMax.txt
+    
+    
     else
 
         local min1=0;
@@ -112,22 +144,32 @@ function findMinMax {
         local minShow2;
         local maxShow2;
 
+        # creating files with UUID to store temporery data computed by the subprocceses.
+        # since child processes can't feed back the results to the parent,the file are used
+        # as an intermediate communicators
         local id=$(getUUID);
         local out1="$TEMP_FOLDER_PATH/out1_$id";
         local out2="$TEMP_FOLDER_PATH/out2_$id";
-        # touch $out1;
-        # touch $out2;
+
+        # double call to the function with each half of the data
         local middle=$((leftIndex+absDif/2));
         findMinMax $leftIndex $middle > "$out1" &
         findMinMax $(($middle+1)) $rightIndex > "$out2" &
+
+        # join-like command that waits for both calls to finish their calculations 
         wait
 
+        # delete temp folders 
         local res1=$(< $out1);
         local res2=$(< $out2);
         rm $out1;
         rm $out2;
         read minShow1 min1 maxShow1 max1 < <(echo $res1);
         read minShow2 min2 maxShow2 max2 < <(echo $res2);
+        
+        # parse result by spliting ths string result according to the char ';'
+        # for each half result
+        
         local parsedRes1;
         IFS=';' read -ra parsedRes1 <<< $res1
         minShow1=${parsedRes1[0]};
@@ -141,8 +183,6 @@ function findMinMax {
         min2=${parsedRes2[1]};
         maxShow2=${parsedRes2[2]};
         max2=${parsedRes2[3]};
-
-        # echo -indexes: $leftIndex $rightIndex res1=[$minShow1 $min1 $maxShow1 $max1] res2=[$minShow2 $min2 $maxShow2 $max2]>> outpulMinMax.txt
         
         if [[ $min1 -lt $min2 ]]
         then
@@ -162,18 +202,16 @@ function findMinMax {
             maxShow=$maxShow2;
         fi
 
-        # echo min1=$min1 max1=$max1 _ min2=$min2 max2=$max2 _ , indexes: $leftIndex $rightIndex, min=$min max=$max _ pid=$$>> outpulMinMax.txt
     fi
 
+    # No return since the func desinged to output a batch of values
     echo "$minShow; $min; $maxShow; $max";
 }
 
 
-
 app=$GET_TVSHOW_TOTAL_LENGTH_BIN #"./GetTvShowTotalLength/bin/Debug/net6.0/GetTvShowTotalLength"
 
-#echo "" > outpulMinMax.txt
-
+# read data fron .txt file and stores it in array line by line
 filename=$(readlink /proc/self/fd/0);
 mapfile -t arr;
 while IFS= read -r line
@@ -181,20 +219,24 @@ do
     arr+= $line; 
 done
 
+# tempFolder stores temporary files which used 
+# by the subprocesses to r/w data
 makeTempFolder;
 
 arrLen=${#arr[@]};
-# echo arr=[${arr[@]}] arrLen=$arrLen
+# call the findMinMax func with the indexes
+# of the first and the last elements of the shows array
 result=$(findMinMax 0 $((arrLen-1)) )
-# echo $result
 rm -r $TEMP_FOLDER_PATH
 
-#parse result
+# parse result by spliting ths string result according to
+#  the char ';'
 IFS=';' read -ra parsedRes <<< $result
 minShow=${parsedRes[0]};
 min=${parsedRes[1]};
 maxShow=${parsedRes[2]};
 max=${parsedRes[3]};
 
+# print to stdout the requested result
 printf "The shortest show: %s (%dh:%dm)\n" "$minShow" "$((min/60))" "$((min%60))"
 printf "The longest show: %s (%dh:%dm)\n" "$maxShow" "$((max/60))" "$((max%60))"
